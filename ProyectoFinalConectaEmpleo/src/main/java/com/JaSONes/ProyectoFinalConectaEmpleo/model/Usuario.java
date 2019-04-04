ff29e5734428eca2637388b7cfb053e5f63b34e5
henry <henry@HHRC>
2019-04-04 19:02:43 +0200

    añadidos paquetes dao y service - Henry - 1902 - 04042019

--- ProyectoFinalConectaEmpleo/src/main/java/com/JaSONes/ProyectoFinalConectaEmpleo/model/Usuario.java
+++ ProyectoFinalConectaEmpleo/src/main/java/com/JaSONes/ProyectoFinalConectaEmpleo/model/Usuario.java
@@ -1,7 +1,45 @@
 package com.JaSONes.ProyectoFinalConectaEmpleo.model;
 
 public class Usuario {
+	int id;
 	private String user;
+	private String password;
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(String user, String password) {
+		super();
+		this.user = user;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	
 
 }
